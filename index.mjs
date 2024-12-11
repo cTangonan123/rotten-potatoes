6f20dd34155f539c0c77ca345dbf9316ba0f0e8d
Ranjita Kaur <111333874+RKaurGitHub@users.noreply.github.com>
2024-12-10 20:34:01 -0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import 'dotenv/config';                           // Import dotenv module
 import mysql from 'mysql2/promise';               // Import mysql module
 import bcrypt from 'bcrypt';                       // Import bcrypt module
 import session from 'express-session';             // Import express-session module
+import fetch from 'node-fetch';                    // Import node-fetch to make external API calls
 
 const app = express();
 
@@ -10,30 +11,30 @@ app.set('port', process.env.PORT || 3000);        // Set the port
 app.set('view engine', 'ejs');                    // Set the view engine
 
 app.use(express.static('public'));                // Serve static files from the public folder
-
 app.use(express.urlencoded({ extended: true }));  // Parse URL-encoded bodies
 app.use(express.json());                          // Parse JSON bodies
 
-// setting up session
+// Setting up session
 app.set('trust proxy', 1); 
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
-  
-}))
+}));
 
-// Setup MySQL connection
+// Setup MySQL connection pool
 const pool = mysql.createPool({                   
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database : process.env.DB_NAME,
+  database: process.env.DB_NAME,
   connectionLimit: 10,
   waitForConnections: true
 });
 
-const conn = await pool.getConnection();
+async function getDbConnection() {
+  return await pool.getConnection();
+}
 
 /* GET Requests */
 
@@ -41,12 +42,6 @@ const conn = await pool.getConnection();
 
 // Handles rendering of the login view
 app.get('/', async (req, res) => {
-  // let sql = 'SELECT * FROM user';
-  // const [rows] = await conn.execute(sql);         
-  // for (let row of rows) {
-  //   console.log(row);                             
-  // }
-  // res.render('index', {"greeting": "Hello, World!", "port": process.env.PORT});, res) => {
   res.render('login');
 });
 
@@ -61,7 +56,7 @@ app.get('/search', isAuthenticated, getWatchListForUser, getPopularMovies, async
   res.render('searchPage', { user_id, watchlist, popularMovies, is_admin, user_name });
 });
 
-// Handles rendering of the searchResults view, once user submits a search query
+// Handles rendering of the searchResults view
 app.get('/search/results', isAuthenticated, getWatchListForUser, getReviewsForUser, async (req, res) => {
   let user_id = req.session.user_id;
   let user_name = req.session.user_name;
@@ -74,17 +69,6 @@ app.get('/search/results', isAuthenticated, getWatchListForUser, getReviewsForUs
   let watched = new Set(watchlist.map(movie => movie.movie_id));
   let reviewed = new Map(reviews.map(review => [review.movie_id, review.id]));
 
-
-  // https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1
-
-  // console.log("this is the user id: " + user_id) // for testing
-  // console.log("this is the watchlist:*********")
-  // for (let movie of watchlist) {
-  //   console.log(movie)
-  // }
-
-  // let user_id = 2;
-  // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
   const url = `https://api.themoviedb.org/3/search/movie?query=${searchQueryURL}&include_adult=false&language=en-US&page=1`;
   const options = {
     method: 'GET',
@@ -97,12 +81,10 @@ app.get('/search/results', isAuthenticated, getWatchListForUser, getReviewsForUs
   let response = await fetch(url, options);
   let data = await response.json();
 
-  
-    
   res.render('searchResults', { "shows": data.results, user_id, user_name, is_admin, watchlist, watched, reviewed });
 });
 
-// Handles rendering of the movieDescription view, once user clicks on a specific movie
+// Handles rendering of the movieDescription view
 app.get('/description', isAuthenticated, getWatchListForUser, getReviewsForUser, async (req, res) => {
   let user_id = req.session.user_id;
   let user_name = req.session.user_name;
@@ -125,27 +107,25 @@ app.get('/description', isAuthenticated, getWatchListForUser, getReviewsForUser,
 
   let response = await fetch(url, options);
   let data = await response.json();
-  console.log(data);
 
   const urlRecommendations = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?language=en-US&page=1`;
   const optionsRecommendations = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
     }
   };
 
-  let respRecommendations = await fetch(urlRecommendations, optionsRecommendations)
+  let respRecommendations = await fetch(urlRecommendations, optionsRecommendations);
   let dataRecommendations = await respRecommendations.json();
 
-  let sql = `
-    SELECT r.user_id, r.movie_id, r.title, r.rating, r.review, u.user_name
-    FROM reviews r
-           LEFT JOIN user u ON r.user_id = u.id
-    WHERE movie_id = ?`;
+  const conn = await getDbConnection();
+  let sql = `SELECT r.user_id, r.movie_id, r.title, r.rating, r.review, u.user_name
+             FROM reviews r
+             LEFT JOIN user u ON r.user_id = u.id
+             WHERE movie_id = ?`;
   const [rows] = await conn.query(sql, [movie_id]);
-  console.log(rows);
 
   res.render('movieDescription', { "show": data, "reviews": rows, user_id, user_name, is_admin, watchlist, watched, reviewed, recommendations: dataRecommendations.results });
 });
@@ -157,12 +137,12 @@ app.get('/userProfile', isAuthenticated, getUserId, checkAdmin, getWatchListForU
   let user_name = req.session.user_name;
   let watchlist = req.session.user_watchlist;
 
-  let sql = `
-    SELECT m.id as movie_id, m.title as movie_title, m.poster_path, r.id as review_id, r.title as review_title, r.rating, review
-    FROM reviews r
-           LEFT JOIN movie m ON r.movie_id = m.id
-    WHERE user_id = ?
-    ORDER BY r.date DESC`;
+  const conn = await getDbConnection();
+  let sql = `SELECT m.id as movie_id, m.title as movie_title, m.poster_path, r.id as review_id, r.title as review_title, r.rating, review
+             FROM reviews r
+             LEFT JOIN movie m ON r.movie_id = m.id
+             WHERE user_id = ?
+             ORDER BY r.date DESC`;
   const [rows] = await conn.query(sql, [user_id]);
 
   res.render('userProfile', { "reviews": rows, user_id, is_admin, user_name, watchlist });
@@ -173,9 +153,9 @@ app.get('/editUsers', getUserId, checkAdmin, getWatchListForUser, async (req, re
   let user_id = req.session.user_id;
   let user_name = req.session.user_name;
   let is_admin = req.session.is_admin;
-  
   let watchlist = req.session.user_watchlist;
 
+  const conn = await getDbConnection();
   let sql = 'SELECT * FROM user';
   const [users] = await conn.query(sql);
   res.render('editUsers', { users, user_id, user_name, is_admin, watchlist });
@@ -183,49 +163,31 @@ app.get('/editUsers', getUserId, checkAdmin, getWatchListForUser, async (req, re
 
 // Handles redirecting to the login page once user logs out
 app.get('/logout', (req, res) => {
-  // req.session.destroy((err) => {
-  //   if (err) {
-  //     return res.redirect('/search');
-  //   }
-  //   res.clearCookie('connect.sid');
-  //   res.redirect('/');
-  // });
   req.session.destroy();
   res.redirect('/');
 });
 
-/* API Specific(associated with client-side js) GET Requests */
+// API Specific GET Requests
 
 // Handles the retrieval of a specific review from the database
 app.get('/getReview/:id', async (req, res) => {
   let review_id = req.params.id;
-  let sql = `
-    SELECT * 
-    FROM reviews 
-    WHERE id = ?`;
+  const conn = await getDbConnection();
+  let sql = `SELECT * FROM reviews WHERE id = ?`;
   const [rows] = await conn.query(sql, [review_id]);
-  console.log(rows[0]);
   res.json(rows[0]);
 });
 
 // Handles username availability check
 app.get('/api/usernameAvailable/:username', async (req, res) => {
   let username = req.params.username;
-  let sql = `
-  SELECT * 
-  FROM user
-  WHERE user_name = ?`;
-  // let sql = `
-  //   SELECT EXISTS
-  //   (SELECT * 
-  //   FROM user
-  //   WHERE user_name = ?)`;
+  const conn = await getDbConnection();
+  let sql = `SELECT * FROM user WHERE user_name = ?`;
   const [rows] = await conn.query(sql, [username]);
-  console.log(rows[0]);
   if (rows.length === 0) {
-    res.json({"available": true});
+    res.json({ "available": true });
   } else {
-    res.json({"available": false});
+    res.json({ "available": false });
   }
 });
 
@@ -234,14 +196,15 @@ app.get('/createNewAccount', (req, res) => {
   res.render('createNewAccount');
 });
 
-
 /* POST Requests */
+
 // Handles the creation of a new account
 app.post("/newUser", async function (req, res) {
   let username = req.body.username;
   let password = req.body.password;
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  const conn = await getDbConnection();
   let sqlSelect = 'SELECT * FROM user WHERE user_name = ?';
   const [rows] = await conn.query(sqlSelect, [username]);
 
@@ -268,323 +231,4 @@ app.post("/newUser", async function (req, res) {
 });
 
 // Handles the login form submission
-app.post('/login', async (req, res) => {
-  const user_name = req.body.user_name;
-  const password = req.body.password;
-
-  let sql = 'SELECT * FROM user WHERE user_name = ?';
-  const [rows] = await conn.query(sql, [user_name]);
-
-  if (rows.length === 0) {
-    res.render('login', { message: 'Invalid username or password' });
-    return;
-  }
-
-  const user = rows[0];
-  console.log(user.password)
-
-  const match = await bcrypt.compare(password, user.password);
-  console.log(match)
-  // const match = password === user.password;
-  if (match) {
-    req.session.user_id = user.id;
-    req.session.user_name = user.user_name;
-    req.session.is_admin = user.is_admin;
-    req.session.authenticated = true;
-    res.redirect('/search');
-  } else {
-    res.render('login', { message: 'Invalid username or password' });
-  }
-});
-
-// Handles the update of a user
-app.post('/updateUser', async (req, res) => {
-  const { id, user_name, is_admin } = req.body;
-  let sql = 'UPDATE user SET user_name = ?, is_admin = ? WHERE id = ?';
-  await conn.query(sql, [user_name, is_admin ? 1 : 0, id]);
-  res.redirect('/editUsers');
-});
-
-// Handles the deletion of a user
-app.post('/deleteUser', async (req, res) => {
-  const { id } = req.body;
-  let sql = 'DELETE FROM user WHERE id = ?';
-  await conn.query(sql, [id]);
-  res.redirect('/editUsers');
-});
-
-// Handles adding a movie to a users watchlist
-app.post('/addToWatchList', getUserId, async (req, res) => {
-  let user_id = req.body.user_id; // hard-coded for now, TODO: change to session user_id
-  // console.log(req.body.movie_id);
-  const movie_id = req.body.movie_id;
-  // check if movie is already in database
-  // if not, add it
-  let sql = `SELECT * FROM movie WHERE id = ?`;
-  const [rows] = await conn.query(sql, [movie_id]);
-  
-  
-  // where we add the movie to the database if it isn't already in there
-  if (rows.length === 0) {
-  const url = `https://api.themoviedb.org/3/movie/${movie_id}?language=en-US`;
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
-    }
-  };
-
-  let result = await fetch(url, options)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      return data;
-      
-    }) 
-    .catch(err => console.error(err));
-
-    sql = `
-      INSERT INTO movie (
-      id, title, release_date, overview, backdrop_path, poster_path, vote_average
-      ) VALUES (
-      ?, ?, ?, ?, ?, ?, ?
-      )`;
-    await conn.query(sql, [result.id, result.title, result.release_date, result.overview, result.backdrop_path, result.poster_path, result.vote_average]);
-  }
-
-  // add the movie to the watchlist
-  
-  let sql2 = `INSERT INTO watchlist (movie_id, user_id) VALUES (?, ?)`;
-  await conn.query(sql2, [movie_id, user_id]); 
-
-  res.json({ message: 'Movie added to your watchlist' });
-});
-
-// Handles removing a movie from users watchlist
-app.post('/removeFromWatchList', getUserId, async (req, res) => {
-  let user_id = req.body.user_id; // hard-coded for now, TODO: change to session user_id
-  const movie_id = req.body.movie_id;
-
-  let sql = `
-    DELETE FROM watchlist 
-    WHERE movie_id = ? AND user_id = ?`;
-  await conn.query(sql, [movie_id, user_id]);
-
-  res.json({ message: 'Movie removed from your watchlist' });
-})
-
-// Handles review form submission
-app.post('/submitReview', getUserId, async (req, res) => {
-  let user_id = req.body.user_id; // hard-coded for now, TODO: change to session user_id
-  
-  // getting the values from the form
-  const { movie_id, title, rating, review } = req.body;
-
-  // check if movie exists in database movie table,
-  let sqlMovie = `SELECT * FROM movie WHERE id = ?`;
-  const [rows] = await conn.query(sqlMovie, [movie_id]);
-
-  // if movie is not in database, pull from TMDB API and insert into database
-  if (rows.length === 0) {
-    const url = `https://api.themoviedb.org/3/movie/${movie_id}?language=en-US`;
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
-      }
-    };
-
-    let result = await fetch(url, options)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        return data;
-        
-      }) // where to do stuff
-      .catch(err => console.error(err));
-    
-    sqlMovie = `
-      INSERT INTO movie (
-      id, title, release_date, overview, backdrop_path, poster_path, vote_average
-    ) VALUES (
-      ?, ?, ?, ?, ?, ?, ?
-    )`;
-    await conn.query(sqlMovie, [result.id, result.title, result.release_date, result.overview, result.backdrop_path, result.poster_path, result.vote_average]);
-  }
-  
-  // check database if user has already submitted a review for this movie
-  let sqlReviewCheck = `
-    SELECT count(*) as count 
-    FROM reviews 
-    WHERE movie_id = ? AND user_id = ?`;
-
-  const [countReview] = await conn.query(sqlReviewCheck, [movie_id, user_id]);
-
-  // if user has already submitted a review, update the review
-  if (countReview[0].count > 0) {
-    let sqlReviewUpdate = `
-      UPDATE reviews 
-      SET title = ?, rating = ?, review = ? 
-      WHERE movie_id = ? AND user_id = ?`;
-
-    await conn.query(sqlReviewUpdate, [title, rating, review, movie_id, user_id])
-  // if user has not submitted a review, insert a new review
-  } else {
-    let sqlReview = `
-      INSERT INTO reviews (
-      movie_id, user_id, title, rating, review
-      ) VALUES (
-      ?, ?, ?, ?, ?)`;
-
-    await conn.query(sqlReview, [movie_id, user_id, title, rating, review])
-      .then(() => console.log('Review submitted!'))
-      
-  }
-    
-  // inserting movie to the watchlist when review is submitted.
-  let sqlWatchListCheck = `
-    SELECT count(*) as count 
-    FROM watchlist 
-    WHERE movie_id = ? AND user_id = ?`;
-  
-  const [countWatchList] = await conn.query(sqlWatchListCheck, [movie_id, user_id]);
-  // if movie is not in watchlist, add it
-  if (countWatchList[0].count === 0) {
-    let sqlWatchList = `
-      INSERT INTO watchlist (
-      movie_id, user_id
-      ) VALUES (
-      ?, ?)`;
-
-    await conn.query(sqlWatchList, [movie_id, user_id])
-      .then(() => console.log('Movie added to watchlist!'))
-      
-  }
-  
-  res.json({ message: 'Database has been updated' });
-});
-
-// Handles review deletion
-app.post('/deleteReview', getUserId, async (req, res) => {
-  let user_id = req.body.user_id; // hard-coded for now, TODO: change to session user_id
-  const review_id = req.body.review_id;
-
-  let sql = `
-    DELETE FROM reviews 
-    WHERE id = ?`;
-  await conn.query(sql, [review_id]);
-
-  res.json({ message: 'Review deleted' });
-});
-
-// Handles password update
-app.post('/updatePassword', getUserId, async (req, res) => {
-  const user_id = req.body.user_id;
-  const newPassword = req.body.password;
-  const hashedPassword = await bcrypt.hash(newPassword, 10);
-
-  let sql = 'UPDATE user SET password = ? WHERE id = ?';
-  await conn.query(sql, [hashedPassword, user_id]);
-
-  res.redirect('/userProfile');
-});
-
-/** MIDDLEWARE Functions **/
-// middleware to check if user is authenticated, apply first to all views that require authentication
-function isAuthenticated(req, res, next) {
-  if (!req.session.authenticated) {
-    res.redirect('/');
-  } else {
-    next();
-  }
-}
-
-// middleware to get watchlist for user
-async function getWatchListForUser(req, res, next) {
-  // get user_id from session
-  let user_id = req.session.user_id; // hard-coded for now, TODO: change to session user_id
-  
-  // get watchlist for user
-  let sql = `
-    SELECT m.id as movie_id, m.title as movie_title, m.poster_path
-    FROM watchlist w
-    LEFT JOIN movie m
-    ON w.movie_id = m.id
-    WHERE w.user_id = ?
-    ORDER BY w.date DESC;
-  `
-  const [rows] = await conn.query(sql, [user_id]);
-  req.session.user_watchlist = rows;
-
-  next();
-  
-}
-
-// middleware to get reviews for user
-async function getReviewsForUser(req, res, next) {
-  // get user_id from session
-  let user_id = req.session.user_id; // hard-coded for now, TODO: change to session user_id
-  
-  // get watchlist for user
-  let sql = `
-    SELECT *
-    FROM reviews r
-    WHERE r.user_id = ?;
-  `
-  const [rows] = await conn.query(sql, [user_id]);
-  req.session.user_reviews = rows;
-
-  next();
-}
-
-// middleware to get popular movies
-async function getPopularMovies(req, res, next) {
-  const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
-    }
-  };
-
-  let response = await fetch(url, options)
-  let data = await response.json();
-  req.session.popularMovies = data.results;
-
-  next();
-}
-
-// deprecated after integrating session
-async function getUserId(req, res, next) {
-  // Get user_id from session
-  let user_id = req.session.user_id;
-  req.body.user_id = user_id;
-  next();
-}
-
-// deprecated after integrating session
-async function checkAdmin(req, res, next) {
-  if (!req.session.user_id) {
-    return res.redirect('/login');
-  }
-
-  let sql = 'SELECT is_admin FROM user WHERE id = ?';
-  const [rows] = await conn.query(sql, [req.session.user_id]);
-
-  if (rows.length > 0 && rows[0].is_admin) {
-    req.body.is_admin = true;
-  } else {
-    req.body.is_admin = false;
-  }
-
-  next();
-}
-
-
-
-app.listen(process.env.PORT, () => {                // Start the server
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
+app.post('/login', async
